278f89016ebac01556aec65d00e65262d36339c2
CJ <ngassajason@yahoo.fr>
2023-03-27 09:43:02 +0100

    resolved dependency issue

--- src/main.js
+++ src/main.js
@@ -2,22 +2,11 @@ import { createApp } from 'vue'
 import './scss/style.scss'
 import App from './App.vue'
 
-/* import "@/assets/css/animate.css";
+import "@/assets/css/animate.css";
 import "@/assets/css/flexslider.css";
 import "@/assets/fonts/icomoon/style.css";
 import "@/assets/css/bootstrap.css";
 import "@/assets/css/style.css";
-
-import "@/assets/js/vendor/jquery.min.js";
-
-import "@/assets/js/vendor/jquery-migrate-3.0.1.min.js";
-import "@/assets/js/vendor/popper.min.js";
-import "@/assets/js/vendor/bootstrap.min.js";
-import "@/assets/js/vendor/jquery.easing.1.3.js";
-import "@/assets/js/vendor/jquery.stellar.min.js";
-import "@/assets/js/vendor/jquery.waypoints.min.js";
-import "https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js";
-import "https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js";
-import "@/assets/js/custom.js"; */
+import "@/assets/js/custom.js";
 
 createApp(App).mount('#app')
